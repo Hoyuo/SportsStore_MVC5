db3ab532a24e19ffd3b9e3537b42966c7d53fcc7
hoyuo <psh9907@gmail.com>
2017-05-07 23:57:17 +0900

    쇼핑카트완성
    모델 바인딩, 카트 완성

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Infrastructure.Binders;
 
 namespace SportsStore.WebUI
 {
@@ -10,6 +12,8 @@ namespace SportsStore.WebUI
 		{
 			AreaRegistration.RegisterAllAreas();
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
 		}
 	}
 }
