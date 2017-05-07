937fdd0ebf5d796b8ba4e495ded3390479edb9a6
hoyuo <psh9907@gmail.com>
2017-05-07 17:59:23 +0900

    카테고리
    카테고리 목록, URL 개선, 네이게이션 메뉴

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -8,16 +8,60 @@ namespace SportsStore.WebUI
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				null,
+				"",
+				new
+				{
+					controller = "Product",
+					action = "List",
+					category = (string)null,
+					page = 1
+				}
+			);
+
 			routes.MapRoute(
 				null,
 				"Page/{page}",
-				new { Controller = "Product", action = "List" }
+				new
+				{
+					controller = "Product",
+					action = "List",
+					category = (string)null
+				},
+				new
+				{
+					page = @"\d+"
+				}
 			);
+
+			routes.MapRoute(
+				null,
+				"{category}",
+				new
+				{
+					controller = "Product",
+					action = "List",
+					page = 1
+				}
+			);
+
 			routes.MapRoute(
 				"Default",
-				"{controller}/{action}/{id}",
-				new { controller = "Product", action = "List", id = UrlParameter.Optional }
+				"{category}/Page/{page}",
+				new
+				{
+					controller = "Product",
+					action = "List"
+				},
+				new
+				{
+					page = @"\d+"
+				}
 			);
+
+			routes.MapRoute(null, "{controller}/{action}");
 		}
 	}
 }
--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -15,11 +15,12 @@ namespace SportsStore.WebUI.Controllers
 			repository = productRepository;
 		}
 
-		public ViewResult List(int page = 1)
+		public ViewResult List(string category, int page = 1)
 		{
 			ProductsListViewModel model = new ProductsListViewModel
 			{
 				Products = repository.Products
+					.Where(p => category == null || p.Category == category)
 					.OrderBy(p => p.ProductID)
 					.Skip((page - 1) * PageSize)
 					.Take(PageSize),
@@ -27,8 +28,11 @@ namespace SportsStore.WebUI.Controllers
 				{
 					CurrentPage = page,
 					ItemPerPage = PageSize,
-					TotalItems = repository.Products.Count()
-				}
+					TotalItems = category == null ?
+						repository.Products.Count() :
+						repository.Products.Where(e => e.Category == category).Count()
+				},
+				CurrentCategory = category
 			};
 			return View(model);
 		}
